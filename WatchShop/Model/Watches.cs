bcba9351e6e249b401e47b06a25d72c7fdea5078
mahrukhimran11@outlook.com <mahrukhimran11@outlook.com>
2022-10-30 15:57:22 +0500

    Add project files.

---
+++ WatchShop/Model/Watches.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WatchesShop.Model
+{
+    public class Watches
+    {
+        [JsonPropertyName("id")]
+        public int watch_id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string watch_name { get; set; }
+
+        [JsonPropertyName("image")]
+        public string watch_image { get; set; }
+
+        public override string ToString()
+        {
+            Watches watches = this;
+            return JsonSerializer.Serialize<Watches>(this);
+        }
+
+    }
+}
